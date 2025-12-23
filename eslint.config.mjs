d78c48b4e1eec742742bb6bedf21097faaf0e513
Eli Silk <37377295+elisilk@users.noreply.github.com>
2025-12-23 17:33:15 -0500

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt(
+  // Your custom configs here
+  {
+    rules: {
+      'vue/html-self-closing': [
+        'error',
+        {
+          html: {
+            void: 'always',
+            normal: 'always',
+            component: 'always',
+          },
+          svg: 'always',
+          math: 'always',
+        },
+      ],
+    },
+  }
+);
